410e51fc3012c7456122f9ddc6b85997a97383c7
Felipe Araújo Silva <asflip-it@protonmail.ch>
2017-07-20 23:04:57 -0300

    Update run.py
    Running through a number of pages
    There is no data structure for storage the response ATM

--- run.py
+++ run.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import time
 import parser
 import requester
 
 from bs4 import BeautifulSoup
 
-BASE_URL = "https://www.gamespot.com/reviews/?review_filter_type[platform]=19&sort=date&page=1"
+BASE_URL = "https://www.gamespot.com/reviews/?review_filter_type[platform]=19&sort=date&page={}"
 
 if __name__ == '__main__':
-    response = requester.capture(BASE_URL)
-    print parser.parse_page(response.content)
+    response = []
+    for page in range(1, 6):
+        page_url = BASE_URL.format(page)
+        page_response = requester.capture(page_url)
+        print 'Requested page {} sucessfull'.format(page_url)
+        time.sleep(3)
+        page_response = parser.parse_page(page_response.content)
+        response.append(page_response)
+
+    print response
